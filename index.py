fd8d7d9c3a1e87c5b9c115cc997f88c4b94ba39d
Daniel <daniel.rojas@utec.edu.pe>
2022-12-18 00:34:47 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,38 @@
+class Solution:
+    def __init__(self):
+        self.solve()
+
+    def number_of_subsequences(self, text: str, sentence: str) -> int:
+        m = len(text)
+        n = len(sentence)
+
+        dp_table = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(n + 1):
+            dp_table[0][i] = 0
+
+        for i in range(m + 1):
+            dp_table[i][0] = 1
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+
+                if text[i - 1] == sentence[j - 1]:
+                    dp_table[i][j] = dp_table[i - 1][j - 1] + dp_table[i - 1][j]
+                else:
+                    dp_table[i][j] = dp_table[i - 1][j]
+
+        return dp_table[m][n]
+
+    def solve(self):
+        p = int(input())
+
+        while p > 0:
+            s = input()
+            t = input()
+            n = self.number_of_subsequences(text=t, sentence=s)
+            print(n)
+            p -= 1
+
+
+Solution()
